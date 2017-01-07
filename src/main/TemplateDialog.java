aebd665bb098f703ebc746859c90962eff708d28
hanuor <hanuortech@gmail.com>
2017-01-07 19:38:45 +0530

    Dialog gui

--- src/main/TemplateDialog.java
+++ src/main/TemplateDialog.java
@@ -12,12 +12,16 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 
 public class TemplateDialog extends JDialog{
 
@@ -26,12 +30,15 @@ public class TemplateDialog extends JDialog{
 		
 		 setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		 setSize(800,800);
-		
+		 JPanel header = new JPanel();
+	        header.setLayout(new BorderLayout());
+	        JLabel topH = new JLabel("Enter a template message below"); 
+	        header.add(topH, BorderLayout.CENTER);
+	        add(header);
          JLabel yMsg = new JLabel();
- 		yMsg.setText("Email Subject");
- 		add(yMsg);
+         Border border = yMsg.getBorder();
+		 Border margin = new EmptyBorder(50,10,50,10);
          JEditorPane temp = new JEditorPane();
-         
  		JScrollPane editorScrollPane = new JScrollPane(temp);
  		editorScrollPane.setVerticalScrollBarPolicy(
  		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -39,6 +46,11 @@ public class TemplateDialog extends JDialog{
  		editorScrollPane.setMinimumSize(new Dimension(10, 10));
        //  temp.setPreferredSize(new Dimension(750, 750));
         add( editorScrollPane, BorderLayout.CENTER);
+        JPanel saveBut = new JPanel();
+        saveBut.setLayout(new BorderLayout());
+        JButton sButton = new JButton("Save"); 
+        saveBut.add(sButton, BorderLayout.CENTER);
+        add(saveBut);
          
          
         

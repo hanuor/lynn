38d4524e88e19c7f1cc8658db8fb7e9b1096c3d9
hanuor <hanuortech@gmail.com>
2017-01-07 14:36:37 +0530

    template dialog

--- src/main/TemplateDialog.java
+++ src/main/TemplateDialog.java
@@ -4,25 +4,33 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dialog;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TemplateDialog extends JDialog{
 
 	public TemplateDialog(JPanel parent) {
 		//super();
 		
-		 setLayout(new BorderLayout());
-         Panel panel = new Panel();
-         panel.add(new Button("Close"));
-         add("South", panel);
-         setSize(500,500);
-
+		 setLayout(new GridLayout(2,1));
+		 setSize(1000,1000);
+		 JPanel panel = new JPanel();
+         setLayout(new BorderLayout());
+         JTextField temp = new JTextField();
+         temp.setPreferredSize(new Dimension(500, 500));
+         panel.add(temp, BorderLayout.CENTER);
+         
+         add(panel);
+         
          addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent){
                dispose();

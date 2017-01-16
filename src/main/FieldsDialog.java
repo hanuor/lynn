fa618fd917c8f503957892c86f61266d9de0afae
hanuor <hanuortech@gmail.com>
2017-01-16 22:43:58 +0530

    Algorithm change

--- src/main/FieldsDialog.java
+++ src/main/FieldsDialog.java
@@ -22,7 +22,7 @@ import core.DatabasePing;
 public class FieldsDialog extends JDialog{
 	private HashMap<String, String> _ans;
 	private String _finalSub;
-	public FieldsDialog(JPanel parent, int subCount, int emailCount, String subText, String emaiText){
+	public FieldsDialog(JPanel parent, int subCount, int emailCount, String subText, String emailText){
 		
 		System.out.println("Get Sub text" +subText);
 		 setLayout(new BorderLayout());
@@ -78,14 +78,14 @@ public class FieldsDialog extends JDialog{
 			save.addActionListener(new ActionListener() { 
 	        	  public void actionPerformed(ActionEvent e) { 
 	        		  _finalSub = null;
-	        		  System.out.println("inders SSSEEE "+ subText);
+	        		//  System.out.println("inders SSSEEE "+ subText);
 	        		  ArrayList<String> answers = new ArrayList<String>();
 	        		  for(int i = 0; i< textFields.size();i++){
 	        			  answers.add(textFields.get(i).getText());
 	        			  //System.out.println("Update   " + textFields.get(i).getText());
 	        		  }
 	        		  ArrayList<String> _arr = new ArrayList<String>();
-	        		  convertToproper(subText, _arr, answers);
+	        		  convertToproper(0,subText, _arr, answers);
 	        		 
 	        		  //System.out.println("Breathing" + .toString());
 	        		  //ArrayList<String> ope = convertToproper(subText, _arr);
@@ -95,8 +95,8 @@ public class FieldsDialog extends JDialog{
 	        		  if(emailCount!=0){
 	        			  
 	        			  
-	        		//  EmailDialog eDialog = new EmailDialog(getContentPane(), emailCount);
-	        		 // _ans = eDialog.showDialog(true);
+	        		EmailDialog eDialog = new EmailDialog(getContentPane(), emailCount, emailText);
+	        		  _ans = eDialog.showDialog(true);
 	        		  setVisible(false);
 	        		  dispose();
 	        		  }else{
@@ -107,18 +107,20 @@ public class FieldsDialog extends JDialog{
 			
 			
 	}
-	public void convertToproper(String _sub, ArrayList<String> _arr, ArrayList<String> answers){
-		//System.out.println("This" + _arr.toString());
+	public void convertToproper(int pivvot, String _sub, ArrayList<String> _arr, ArrayList<String> answers){
+		System.out.println("This    dsa   " + _sub	);
 		int beginIndex = _sub.indexOf("#*#");
+		System.out.println("Keynote   " + beginIndex);
 		int newIndex = beginIndex+3;
 		int pivotIndex = 0;
 		
 		String newS = null;
 
 		String remS = null;
+		if(beginIndex!=-1){
 		if(newIndex > (_sub.length()-1)){
 			StringBuilder buildStr = new StringBuilder();
-		
+		System.out.println("   JJJJ     " + _arr.toString() + " and  sixe is   " + _arr.size());
 			_arr.add(_sub.substring(pivotIndex, beginIndex+3));
 			 for(int i=0; i< _arr.size(); i++){
 				 buildStr.append(_arr.get(i).replace("#*#", answers.get(i).toString()));
@@ -127,14 +129,30 @@ public class FieldsDialog extends JDialog{
 			System.out.println("Heya a " + _arr);
 			return;
 		}else{
-			newS = _sub.substring(pivotIndex, beginIndex+3);
-			remS = _sub.substring(beginIndex + 3);
 			
-			pivotIndex = beginIndex+3;
+			newS = _sub.substring(pivotIndex, beginIndex+3);
+			
+			System.out.println("   Ye lo " + newS+"   " + pivvot);
+			pivvot = beginIndex+3;
+			
+			System.out.println("   Ye lo  aur  " + newS+"   " + pivvot);
+			remS = _sub.substring(pivvot);
 			_arr.add(newS);
-		}
-		
-		convertToproper(remS, _arr, answers);
+							}
+			}else{
+				StringBuilder buildStr = new StringBuilder();
+				System.out.println("   JJJJ     " + _sub + "  pivvot " );
+					 System.out.println("Queens Bby " + _arr.toString());
+					
+					 for(int i=0; i< _arr.size(); i++){
+						 buildStr.append(_arr.get(i).replace("#*#", answers.get(i).toString()));
+		     		  }
+					 buildStr.append(_sub);
+					 System.out.println("Queens Bby " + buildStr.toString());
+					System.out.println("Heya a " + _arr);
+					return;
+			}
+		convertToproper(pivvot, remS, _arr, answers);
 	}
 	public HashMap<String, String> showDialog(boolean check){
 		return _ans;

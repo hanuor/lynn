b63c8668f8b78c4ca5292d38bc2d221edf8e8017
hanuor <hanuortech@gmail.com>
2017-01-22 12:47:21 +0530

    Edit dialog

--- src/main/EditDialog.java
+++ src/main/EditDialog.java
@@ -1,17 +1,40 @@
 package main;
 
+import java.util.ArrayList;
+
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import core.ControlPanelMethods;
+
 public class EditDialog extends JDialog{
+	ArrayList<String> _retArr;
 	public EditDialog(JFrame parent){
 		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		JTextField jj = new JTextField();
 		add(jj);
 		
+		 DefaultListModel<String> listModel = new DefaultListModel<>();
+         
+         _retArr = ControlPanelMethods.getList();
+         if(_retArr == null){
+       	 // System.out.println("HEre!!!");
+       	  _retArr = new ArrayList<String>();
+       	  _retArr.add("Nothing here. Click on 'Add a template' to add templates. Or click refresh");
+       	  for(int i = 0; i< _retArr.size(); i++){
+       		  listModel.addElement(_retArr.get(i).toString());
+       	  }
+         }else{
+
+       	  System.out.println("Noit nul");
+       	  for(int i = 0; i< _retArr.size(); i++){
+       		  listModel.addElement(_retArr.get(i).toString());
+       	  }
+         }
 	}
 	
 	public int  showDialog(){
